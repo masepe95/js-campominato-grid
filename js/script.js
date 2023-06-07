b5ceef36e0d1bc74b816cc5cabf9cfde2d5aea79
masepe95 <marcosepe10@gmail.com>
2023-06-07 23:14:19 +0200

    [JS] numbered grid generated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 console.log('JS OK') 
+
+// ! OPERAZIONI INIZIALI
+
+// Recupero elementi 
+const gridField = document.getElementById('grid');
+const playButton = document.querySelector('button');
+const difficultySelector = document.getElementById('difficulty');
+
+// Dichiaro dati griglia
+const hardGame = 49;
+const mediumGame= 81;
+const easyGame = 100;
+
+
+//funzione arrow per creare cell
+const createCell = () => {
+    const cell = document.createElement('div');
+    cell.className = 'cellEasy';
+
+    return cell;
+}
+// Costruiamo la griglia in DOM al click del bottone
+playButton.addEventListener('click', function() {
+        for (let i = 0; i < easyGame; i++){
+            const cell = createCell();
+
+            cell.innerText = i + 1;
+            
+            cell.addEventListener('click', () => {
+                cell.classList.toggle('clicked');
+            });
+
+            gridField.appendChild(cell);
+        }
+    })
+
+
